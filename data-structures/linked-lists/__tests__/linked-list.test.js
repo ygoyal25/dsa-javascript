b62542b4e485eb8aa60baaf254b06121467ae436
yash2503 <yashendra.goyal@peppercloud.com>
2020-08-14 15:12:37 +0530

    test commit

--- data-structures/linked-lists/__tests__/linked-list.test.js
+++ data-structures/linked-lists/__tests__/linked-list.test.js
@@ -2,7 +2,6 @@ import LL from "../linked-list";
 import findMiddle from "../../../algorithms/linked-list/find-middle";
 
 test('create a linked list', () => {
-    // expect(sum(1, 2)).toBe(3);
     const list = new LL();
     list.append(2);
     list.append(5);
