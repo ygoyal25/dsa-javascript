26c222a815215b167e1e5168e1075ae8527a859f
ygoyal25 <goyal.yashendra@gmail.com>
2021-02-20 10:33:57 +0530

    find sunstring of given string

---
+++ algorithms/strings/subset-of-string/subset-of-string.js
@@ -0,0 +1,19 @@
+function subSet(str) {
+    let totalBits = str.length;
+    const totalSubStrs = Math.pow(2,totalBits);
+    let i = 0;
+    while(i < totalSubStrs) {
+        let currBit = 0;
+        let subStr = ""
+        while(currBit < totalBits) {
+            if ((1 << currBit) & i) {
+                subStr += str[currBit];
+            }
+            currBit++;
+        }
+        console.log(subStr);
+        i++
+    }
+}
+
+subSet("abd");
